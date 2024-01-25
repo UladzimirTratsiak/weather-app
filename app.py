726a27b5668ca30cabe0ac22597e4821962184a3
Volodia <you@example.com>
2024-01-25 19:42:35 +0100

    Create Dockerfile; fix the app.py file

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import os
 
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 from wtforms import StringField, SubmitField
 
 from weather import get_weather
 
 app = Flask(__name__, template_folder="templates")
+csrf = CSRFProtect(app)
 
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
@@ -25,9 +27,10 @@ def index():
         weather_data = get_weather(city)
 
         return render_template("index.html", data=weather_data, form=form)
-
-    return render_template("index.html", data={}, form=form)
+    # Temporary plug
+    data = get_weather("Wroclaw")
+    return render_template("index.html", data=data, form=form)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
